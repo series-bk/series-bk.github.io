f8ef10e0095d982e7d007edaa2f7d2d65b980baa
yyy-anime <haygontt@gmail.com>
2025-08-29 21:10:30 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -1,6 +1,12 @@
 const animeList = [
   
           
+    {
+        "title": "ทะลุมิติมาสะสมรัก ตอนที่ 1-74 พากย์ไทย",
+        "img": "https://img5.pic.in.th/file/secure-sv1/--2025-511.webp",
+        "type": "ตอนที่ 1-74 พากย์ไทย",
+        "link": "https://series-bk.github.io/Transmigrated-to-Collect-Love-Thai-dubbed.html"
+    },
    
 
   // เพิ่มรายการอนิเมะได้เรื่อย ๆ
