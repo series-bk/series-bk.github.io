7c30d3b8ffd64e26aff8545e7a2e2e332bbac1bf
series-bk <haygontt+github01@gmail.com>
2026-09-06 09:35:44 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -8,7 +8,140 @@ const animeList = [
     },
 
     
-   
+        {
+        "title": "ทางสู่มหาเศรษฐีโลก พากย์ไทย",
+        "img": "https://series-love.com/api/img/51bf400bd897f440eb11.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/The-Path-to-Becoming-a-World-Billionaire.html"
+    },
+
+     {
+        "title": "เจ้าหญิงในกรงรัก พากย์ไทย",
+        "img": "https://series-love.com/api/img/cee2e3b036de6e643a14.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/The-Princess-in-the-Cage-of-Love.html"
+    },
+
+     {
+        "title": "ราชินีแวมไพร์ขอเอาคืน พากย์ไทย",
+        "img": "https://series-love.com/api/img/ace49f03559674826ca5.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/The-Vampire-Queen-Takes-Revenge.html"
+    },
+
+     {
+        "title": "ผิดนัดครั้งที่ 18 ฉันจะไม่รออีกแล้ว พากย์ไทย",
+        "img": "https://series-love.com/api/img/f87278f55f4639f3135b.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/The-18th-Broken-Promise-I-Will-Not-Wait-Anymore.html"
+    },
+
+
+     {
+        "title": "สลับเจ้าบ่าว ฉันได้สามีเป็นบอสมาเฟีย พากย์ไทย",
+        "img": "https://series-love.com/api/img/e70ef0ea4922597249e8.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/Swapping-the-Groom-I-Married-the-Mafia-Boss.html"
+    },
+
+     {
+        "title": "นั่วหน้วลงมาจากภูเขาและเหล่าศิษย์ของเธอก็แตกตื่น พากย์ไทย",
+        "img": "https://series-love.com/api/img/c4d627b01ec8409c0f99.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/Nuwa-Comes-Down-from-the-Mountain.html"
+    },
+
+     {
+        "title": "รักนี้ไม่มีวันให้อภัย พากย์ไทย",
+        "img": "https://series-love.com/api/img/72be12f7732c7cb6cddb.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/This-Love-Will-Never-Be-Forgiven.html"
+    },
+
+     {
+        "title": "วันที่คะแนนเอาชีวิตรอดของฉันกลายเป็นศูนย์ พากย์ไทย",
+        "img": "https://series-love.com/api/img/def6419d9e02528297f0.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/The-Day-My-Survival-Score-Hit-Zero.html"
+    },
+
+     {
+        "title": "พวกเขาเรียกมันว่าความยุติธรรม พากย์ไทย",
+        "img": "https://series-love.com/api/img/8cbf869277085fd510e5.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/They-Called-It-Justice.html"
+    },
+
+     {
+        "title": "เกิดใหม่กับ ระบบกู้ชีวิต พากย์ไทย",
+        "img": "https://series-love.com/api/img/079eb0f65987fced01d8.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/Reborn-with-the-Rescue-System.html"
+    },
+
+     {
+        "title": "แม่ของคู่หมั้นที่นอกใจฉันคือลิลิธ พากย์ไทย",
+        "img": "https://series-love.com/api/img/44ba3ab6dd594ffaa8a4.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/My-Cheating-Fiances-Mother-Is-Lilith.html"
+    },
+
+     {
+        "title": "ราชินีที่ถูกปฎิเสธจากดอนมาเฟีย พากย์ไทย",
+        "img": "https://series-love.com/api/img/88ca5ed812726aa71131.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/The-Queen-Rejected-by-the-Mafia-Don.html"
+    },
+
+     {
+        "title": "ราชาหมาป่าขี้เมา พากย์ไทย",
+        "img": "https://series-love.com/api/img/bada3710eaa245be933a.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/The-Drunken-Wolf-King.html"
+    },
+
+     {
+        "title": "เริ่มด้วยแสดง จบด้วยรักจริง พากย์ไทย",
+        "img": "https://series-love.com/api/img/5bb985783dc9f0cd24d2.webp",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/It-Started-as-an-Act-and-Ended-as-Real-Love.html"
+    },
+
+     {
+        "title": "ชาติก่อนแค้นรัก ชาตินี้แปลกหน้า พากย์ไทย",
+        "img": "https://series-love.com/api/img/558ce7026bfa5831d79b.webp",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/Past-Life-Love-and-Hate-This-Life-Strangers.html"
+    },
+
+     {
+        "title": "พ่อขอโทษที่กลับมาช้าไป พากย์ไทย",
+        "img": "https://series-love.com/api/img/4c73b8f67dad3a984704.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/Sorry-Father-Came-Home-Too-Late.html"
+    },
+
+     {
+        "title": "เจ้าชายแวมไพร์คลั่งรัก พากย์ไทย",
+        "img": "https://series-love.com/api/img/f82fa378577b72e47710.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/The-Vampire-Prince-Madly-in-Love.html"
+    },
+
+     {
+        "title": "พ่อของข้าคือราชันมังกร พากย์ไทย",
+        "img": "https://series-love.com/api/img/754c383a21192eb9ac82.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/My-Father-Is-the-Dragon-King.html"
+    },
+
+     {
+        "title": "เจ้าสาว ของอัลฟ่าอำมหิต พากย์ไทย",
+        "img": "https://series-love.com/api/img/0663fac22e6bfaa83bd6.jpg",
+        "type": "พากย์ไทย",
+        "link": "https://series-bk.github.io/The-Bride-of-the-Ruthless-Alpha.html"
+    }
+
 
   // เพิ่มรายการอนิเมะได้เรื่อย ๆ
 ];
